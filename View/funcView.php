718a3407971174f43aef06e8ca9c0e5f915c0903
Jonathan Narvaez <jonathanarvaez@msn.com>
2018-02-27 10:32:21 -0500

    Primer php en carpeta View

---
+++ View/funcView.php
@@ -0,0 +1,25 @@
+<?php
+
+function inicio(){
+    echo '<html>';
+}
+
+function head(){
+    echo
+   '<head>
+        <meta charset="windows-1252">
+        <title>EnLinea&#174; PWA</title>
+        <link rel="manifest" href="manifest.json">
+        <meta name="theme-color" content="#0D47A1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="Images/favicon.ico" type="image/x-icon"/>
+         <!-- Hojas de Estilo (BEGIN) -->
+        <link href="StyleSkin/fontawesome.css" rel="stylesheet" />
+        <link href="StyleSkin/StyleSkin.css" rel="stylesheet" type="text/css" />
+    
+        <!-- Hoja de Estilo (END) -->
+    </head>';
+}
+
+
+?>
