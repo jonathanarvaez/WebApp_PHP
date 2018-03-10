a6ebee8471b3650e9da1e22259f5901c4270cc7f
Jonathan Narvaez <jonathanarvaez@msn.com>
2018-03-10 09:03:21 -0500

    Cambiando Iconos

--- View/funcView.php
+++ View/funcView.php
@@ -15,7 +15,7 @@ function inicio()
             <link rel="manifest" href="manifest.json">
             <meta name="theme-color" content="#0D47A1">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="icon" href="Images/favicon.ico" type="image/x-icon"/>
+            <link rel="icon" href="Images/IconosEnLinea/ICO/favicon.ico" type="image/x-icon"/>
             <!-- Hojas de Estilo (BEGIN) -->
                 <link href="StyleSkin/fontawesome.css" rel="stylesheet" />
                 <link href="StyleSkin/StyleSkin.css" rel="stylesheet" type="text/css" />
@@ -36,7 +36,7 @@ function barraTop()
         <table id="N0BarraTop-Tabla" class="barraTop-Table">
             <tr>
                 <td class="barraTop-Table-Menu-s barraTop-Table-Menu-m barraTop-Table-Menu-f" onClick="eventosWebEstatica.prototype.BotonClicEfecto(1,\'N0Menu\', \'N0Auxiliar\',\'N0Contenido\')"> <i class="fa fa-bars"></i> </td>
-                <td class="barraTop-Table-Menu-s barraTop-Table-Menu-m barraTop-Table-Menu-f" title="Software de Gestion Empresarial"> <img src="Images/logo.png" alt="logo" class="barraTop-logo-s barraTop-logo-m barraTop-logo-f"> </td>
+                <td class="barraTop-Table-Menu-s barraTop-Table-Menu-m barraTop-Table-Menu-f" title="Software de Gestion Empresarial"> <img src="Images/IconosEnLinea/PNG/EnLinea_T_PNG_512.png" alt="Software De Gestion Empresarial" class="barraTop-logo-s barraTop-logo-m barraTop-logo-f"> </td>
                 <td class="barraTop-Table-Titulo-s barraTop-Table-Titulo-m barraTop-Table-Titulo-f">Consulta de pacientes</td>
                 <th class="barraTop-Table-Espaciador-s barraTop-Table-Espaciador-m barraTop-Table-Espaciador-f"></th>
                 <td class="barraTop-Table-Boton-s barraTop-Table-Boton-m barraTop-Table-Boton-f" title="Cargando..."> <i class="fa fa-spinner fa-pulse fa-3x fa-fw ico-cargando-s ico-cargando-m ico-cargando-f" aria-hidden="true"></i> </td>
